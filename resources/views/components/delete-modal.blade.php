a2e67dea8209ba8cec67ac2726450f9a3eafb26d
Irwin Madet <irwin.madet@gmail.com>
2021-02-25 22:44:01 +0100

    Reworking of delete modal and toast

--- resources/views/components/delete-modal.blade.php
+++ resources/views/components/delete-modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="delete-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -6,11 +6,11 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                Are you sure you want to delete this media?
+                {{ $message }}
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">No</button>
-                <button type="button" class="btn btn-danger btn-confirm btn-icon"><i data-feather="trash-2"></i>Yes</button>
+                <button type="button" class="btn btn-danger btn-confirm btn-icon" data-bs-dismiss="modal"><i data-feather="trash-2"></i>Yes</button>
             </div>
         </div>
     </div>
--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,5 +1,5 @@
 <div class="position-fixed end-0 px-3" style="z-index: 5">
-    <div class="toast fade" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast slide fade hide" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
             <strong class="me-auto">Information</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
