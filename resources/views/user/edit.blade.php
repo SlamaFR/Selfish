7ad78594186c2d087e8e0ab6059c2bff2032c08f
Irwin Madet <irwin.madet@gmail.com>
2021-03-02 23:35:59 +0100

    Add disk quota to user settings

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -24,22 +24,22 @@
     @include('flash::message')
     <div class="row">
         <div class="col-8">
-            <div class="card">
+            <div class="card mb-4">
                 <div class="card-header">
-                    Profile summary
+                    User settings
                 </div>
                 <div class="card-body">
                     <form action="{{ $self ? route('user.settings.info') : route('admin.user.settings.info', ['userId' => $user->id]) }}" method="POST">
                         @csrf
                         <div class="row mb-3">
-                            <label class="col-sm-3 col-form-label">User code</label>
-                            <div class="col-sm-9">
+                            <label class="col-sm-4 col-form-label">User code</label>
+                            <div class="col">
                                 <input type="text" value="{{ $user->code }}" class="form-control" readonly>
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-3 col-form-label">Username</label>
-                            <div class="col-sm-9">
+                            <label class="col-sm-4 col-form-label">Username</label>
+                            <div class="col">
                                 <input name="username" type="text" value="{{ $user->username }}" class="form-control @error('username') is-invalid @enderror">
                                 @error('username')
                                     <span class="invalid-feedback text-start ms-1 mb-1" role="alert">
@@ -49,8 +49,8 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-3 col-form-label">Email address</label>
-                            <div class="col-sm-9">
+                            <label class="col-sm-4 col-form-label">Email address</label>
+                            <div class="col">
                                 <input name="email" type="email" value="{{ $user->email }}" class="form-control @error('email') is-invalid @enderror">
                                 @error('email')
                                     <span class="invalid-feedback text-start ms-1 mb-1" role="alert">
@@ -60,25 +60,69 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-3 col-form-label">Personnal token</label>
-                            <div class="col-sm-9">
+                            <label class="col-sm-4 col-form-label">Personnal token</label>
+                            <div class="col">
                                 <div class="input-group">
                                     <input type="password" class="form-control hover-to-see" value="{{ $user->access_token }}" id="personnal-token" readonly>
                                     <button class="btn btn-icon btn-outline-secondary copy" type="button" data-clipboard-target="#personnal-token">
                                         <i data-feather="clipboard"></i>Copy
                                     </button>
-                                    <button class="btn btn-icon btn-outline-primary" type="button" id="regenerate-token">
+                                    <button class="btn btn-icon btn-outline-primary" type="button" data-action="regenerate-token" @if(!$self) data-id="{{ $user->id }}" @endif>
                                         <i data-feather="refresh-cw"></i>Regenerate
                                     </button>
                                 </div>
                                 <small class="text-muted">Hover to reveal token.</small>
                             </div>
                         </div>
+                        <hr>
+                        <div class="row mb-3">
+                            <label class="col-sm-4 col-form-label">Maximum disk quota</label>
+                            <div class="col my-2">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="disk_max-quota" id="disk_max-quota_default" value="default" @if($user->settings()->get('disk.max_quota') == 'default') checked @endif @if(!Auth::user()->admin) disabled @endif>
+                                    <label class="form-check-label" for="disk_max-quota_default">Default</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="disk_max-quota" id="disk_max-quota_custom" value="custom" @if($user->settings()->get('disk.max_quota') == 'custom') checked @endif @if(!Auth::user()->admin) disabled @endif>
+                                    <label class="form-check-label" for="disk_max-quota_custom">Custom</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-sm-4 col-form-label">Custom disk quota</label>
+                            <div class="col">
+                                <div class="input-group">
+                                    <input type="number" name="disk_custom-max-quota" id="disk_custom-max-quota" class="form-control" min="0" @if($user->settings()->get('disk.max_quota') == 'default' || !Auth::user()->admin) disabled @endif value="{{ $maxDiskQuota >> $maxDiskQuotaShift }}">
+                                    <select name="disk_custom-max-quota_unit" id="disk_custom-max-quota_unit" class="form-select form-control" @if($user->settings()->get('disk.max_quota') == 'default' || !Auth::user()->admin) disabled @endif>
+                                        @foreach(['bytes', 'KiB', 'MiB', 'GiB'] as $index => $unit)
+                                            <option value="{{ $index * 10 }}" @if($maxDiskQuotaShift == $index * 10) selected @endif>{{ $unit }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <small class="m-0 text-muted">0 means no limit.</small>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-sm-4 col-form-label">Auto-delete</label>
+                            <div class="col my-auto">
+                                <div class="pt-2">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="disk_auto-delete" id="disk_auto-delete_enabled" value="1" @if($user->settings()->get('disk.auto_delete') == '1') checked @endif>
+                                        <label class="form-check-label" for="disk_auto-delete_enabled">Enabled</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="disk_auto-delete" id="disk_auto-delete_disabled" value="0" @if($user->settings()->get('disk.auto_delete') == '0') checked @endif>
+                                        <label class="form-check-label" for="disk_auto-delete_disabled">Disabled</label>
+                                    </div>
+                                </div>
+                                <small class="m-0 text-muted">Older files will automatically be deleted when exceeding disk quota.</small>
+                            </div>
+                        </div>
                         <button type="submit" class="btn btn-icon btn-outline-success"><i data-feather="save"></i>Save</button>
                     </form>
                 </div>
             </div>
-            <div class="card mt-4">
+            <div class="card mb-4">
                 <div class="card-header">
                     Change password
                 </div>
@@ -204,7 +248,7 @@
                         You can download it <a href="https://getsharex.com/">here</a>.
                     </p>
                     <div class="d-grid">
-                        <button class="btn btn-icon btn-outline-{{ Cookie::get('theme', 'dark') == 'dark' ? 'light' : 'dark' }}" @if($self) data-action="download-sharex" @else disabled @endif>
+                        <button class="btn btn-icon btn-outline-{{ Cookie::get('theme', Setting::get('app.default_theme')) == 'dark' ? 'light' : 'dark' }}" @if($self) data-action="download-sharex" @else disabled @endif>
                             <i data-feather="download"></i>Download ShareX configuration
                         </button>
                     </div>
@@ -213,10 +257,4 @@
         </div>
     </div>
 </div>
-@endsection
-
-@section('script')
-<script>
-    const tokenUpdateMessage = "{{ $self ? "Your personnal access has been regenerated and copied to clipboard." : $user->username . "'s personnal access token has been regenerated and copied to clipboard." }}";
-</script>
 @endsection
